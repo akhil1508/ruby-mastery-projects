71010732cb61162ffb093900fc1c0d524eec1dd6
Akhil <akhil.potukuchi@gmail.com>
2016-12-02 13:53:31 +0530

    Added substrings

---
+++ substrings/substrings.rb
@@ -0,0 +1,30 @@
+def substrings (word, dictionary)
+  valid_substrings = {}
+  words= word.split(" ")
+  puts dictionary.inspect
+  puts words.inspect
+ words.each do |current_word|
+
+   dictionary.each do |current_dictionary_word|
+
+     valid_substrings[current_dictionary_word] = 0
+       if current_word.match(/[current_dictionary_word]/)
+
+         valid_substrings[current_dictionary_word]+=1
+
+       end
+
+
+
+ end
+
+end
+return valid_substrings
+end
+dictionary = []
+puts"Enter word"
+word = gets.chomp
+puts"Enter dictionary(comma separated values)"
+dictionary=gets.chomp.split(",")
+valid_substrings = substrings(word, dictionary)
+puts valid_substrings
